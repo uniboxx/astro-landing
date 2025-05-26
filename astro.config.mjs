3abda773cc056d54a8ee93982e57454198f60225
unibox <unibox@duck.com>
2025-05-26 16:54:15 +0200

    add sitemap and astro-seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,11 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-landingpage.netlify.app/',
-  integrations: [sitemap()],
+  integrations: [
+    sitemap({
+      filter: (page) =>
+        page !== 'https://stargazers.club/secret-vip-lounge/' &&
+        page !== 'https://stargazers.club/secret-vip-lounge-2/',
+    }),
+  ],
 });
