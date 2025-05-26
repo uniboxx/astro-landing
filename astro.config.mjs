81c55e74ace3accbec8c2e899c38de6bae61d374
unibox <unibox@duck.com>
2025-05-26 16:13:13 +0200

    edit site in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http:localhost:4321',
+  site: 'https://astro-landingpage.netlify.app/',
   integrations: [sitemap()],
 });
